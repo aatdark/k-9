ccb4810b0fd5401fadda11c57861c78f41d8c3d6
Bernhard Redl <bernhardredl@inode.at>
2011-05-16 15:44:25 +0200

    refactored PGP MimeDetection from MessageView to CryptoProvider

--- src/com/fsck/k9/crypto/CryptoProvider.java
+++ src/com/fsck/k9/crypto/CryptoProvider.java
@@ -7,6 +7,9 @@ import android.net.Uri;
 
 import com.fsck.k9.mail.Body;
 import com.fsck.k9.mail.Message;
+import com.fsck.k9.mail.MessagingException;
+import com.fsck.k9.mail.Part;
+import com.fsck.k9.mail.internet.MimeUtility;
 
 /**
  * A CryptoProvider provides functionalities such as encryption, decryption, digital signatures.
@@ -41,4 +44,38 @@ abstract public class CryptoProvider {
     abstract public boolean decryptPgpMime(Activity mActivity, Uri uri, PgpData pgpData);
     abstract public boolean encrypt(Activity activity, Body body, String contentType, PgpData pgpData);
     
+    /**
+     * searches for useable MimeParts and then calls decrypt with the extracted
+     * data. It is save to call this method on already encrypted | verfied
+     * emails (it will not perform any action again)
+     *
+     * @param activity
+     *            the activity
+     * @param message
+     *            the message to be parsed
+     * @param pgpData
+     *            the current PGP state to check if it has already verified
+     * @return true if already verified | decoded ; false if it is not a PGP msg
+     *         and otherwise the return value of decrypt
+     * @throws MessagingException
+     */
+    public boolean decrypt(Activity activity, Message message, PgpData pgpData) throws MessagingException {
+        if (pgpData.getDecryptedData() != null) {
+            return true;
+        }
+        if (!isSigned(message) && !isEncrypted(message)) {
+            return false;
+        }
+        String data = null;
+        Part part;
+        part = MimeUtility.findFirstPartByMimeType(message, "text/plain");
+
+        if (part == null) {
+            part = MimeUtility.findFirstPartByMimeType(message, "text/html");
+        }
+        if (part != null) {
+            data = MimeUtility.getTextFromPart(part);
+        }
+        return decrypt(activity, data, pgpData);
+    }
 }
